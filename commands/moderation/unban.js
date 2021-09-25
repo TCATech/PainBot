3b2edd0ff1c2b5631b9e5e9a6165cbaa6d1bff10
Not TCA <theabcashow.josh@gmail.com>
2021-09-25 21:02:02 +0800

    added try catch in unban command

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -12,22 +12,28 @@ module.exports = {
   run: async (client, message, args) => {
     const userID = args[0];
 
-    message.guild.members.unban(userID).then((user) => {
-      const embed = new MessageEmbed()
-        .setTitle("Oh great he's screaming again.")
-        .setDescription(
-          `I have successfully unbanned **${user.tag}** from the server!`
-        )
-        .setColor(message.color)
-        .setFooter(
-          client.user.username,
-          client.user.displayAvatarURL({ dynamic: true })
-        )
-        .setTimestamp();
+    try {
+      message.guild.members.unban(userID).then((user) => {
+        const embed = new MessageEmbed()
+          .setTitle("Oh great he's screaming again.")
+          .setDescription(
+            `I have successfully unbanned **${user.tag}** from the server!`
+          )
+          .setColor(message.color)
+          .setFooter(
+            client.user.username,
+            client.user.displayAvatarURL({ dynamic: true })
+          )
+          .setTimestamp();
 
-      message.reply({
-        embeds: [embed],
+        message.reply({
+          embeds: [embed],
+        });
       });
-    });
+    } catch {
+      message.reply({
+        content: "❌ That user isn't banned in this server.",
+      });
+    }
   },
 };
