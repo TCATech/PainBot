5e49cedaa29d87002e4426ff249c4d9d623d934e
Not TCA <theabcashow.josh@gmail.com>
2021-10-01 09:53:22 +0800

    finished help command, finally

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,4 +1,10 @@
-const { Client, Message } = require("discord.js");
+const {
+  Client,
+  Message,
+  MessageEmbed,
+  MessageActionRow,
+  MessageSelectMenu,
+} = require("discord.js");
 
 module.exports = {
   name: "help",
@@ -8,5 +14,101 @@ module.exports = {
    * @param {Client} client
    * @param {Message} message
    */
-  run: async (client, message) => {},
+  run: async (client, message) => {
+    const directories = [
+      ...new Set(client.commands.map((cmd) => cmd.directory)),
+    ];
+
+    /**
+     *
+     * @param {String} str
+     * @returns string
+     */
+    const formatString = (str) =>
+      `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+
+    const categories = directories.map((dir) => {
+      const getCommands = client.commands
+        .filter((cmd) => cmd.directory === dir)
+        .map((cmd) => {
+          return {
+            name: cmd.name || "???",
+            description: cmd.description || "no description for this command",
+          };
+        });
+
+      return {
+        directory: formatString(dir),
+        commands: getCommands,
+      };
+    });
+
+    const embed = new MessageEmbed().setDescription(
+      "Please choose a category using the dropdown menu below."
+    );
+
+    /**
+     *
+     * @param {Boolean} state
+     * @returns MessageActionRow
+     */
+    const components = (state) => [
+      new MessageActionRow().addComponents(
+        new MessageSelectMenu()
+          .setCustomId("help-menu")
+          .setPlaceholder("Category...")
+          .setDisabled(state)
+          .addOptions(
+            categories.map((cmd) => {
+              return {
+                label: cmd.directory,
+                value: cmd.directory.toLowerCase(),
+                description: `Commands from the ${cmd.directory} category`,
+              };
+            })
+          )
+      ),
+    ];
+
+    const init = await message.reply({
+      embeds: [embed],
+      components: components(false),
+    });
+
+    const filter = (int) => int.user.id === message.author.id;
+
+    const collector = message.channel.createMessageComponentCollector({
+      filter,
+      componentType: "SELECT_MENU",
+      time: 30 * 1000,
+    });
+
+    collector.on("collect", (int) => {
+      const [directory] = int.values;
+      const category = categories.find(
+        (x) => x.directory.toLowerCase() === directory
+      );
+
+      const categoryEmbed = new MessageEmbed()
+        .setTitle(`${formatString(directory)} commands`)
+        .setDescription(
+          `Here is a list of commands from the ${directory} category.`
+        )
+        .addFields(
+          category.commands.map((cmd) => {
+            return {
+              name: `\`${cmd.name}\``,
+              value: cmd.description,
+              inline: true,
+            };
+          })
+        );
+
+      int.update({ embeds: [categoryEmbed] });
+    });
+
+    collector.on("end", () => {
+      init.edit({ components: components(true) });
+    });
+  },
 };
