6624e4b5e584994964e344e5e595b2a802b6ef38
Not TCA <theabcashow.josh@gmail.com>
2021-10-01 08:36:35 +0800

    added random status

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,21 @@
 const client = require("../index");
 
 client.on("ready", () => {
+  setInterval(() => {
+    const list = [
+      ">help | PainBot.tk",
+      `${client.users.cache.size} users | PainBot.tk`,
+      `${client.guilds.cache.size} servers | PainBot.tk`,
+      `${client.channels.cache.size} channels | PainBot.tk`,
+    ];
+    const randomStatus = list[Math.floor(Math.random() * list.length)];
+    let statusType = "WATCHING";
+    if (randomStatus === ">help | PainBot.tk") {
+      statusType = "LISTENING";
+    }
+
+    client.user.setActivity(randomStatus, { type: statusType });
+  }, 10000);
+
   console.log(`${client.user.tag} is now online!`);
 });
