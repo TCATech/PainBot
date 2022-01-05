4664dff723df3843e220cbfcc218e633d24a6dd9
Not TCA <theabcashow.josh@gmail.com>
2022-01-05 12:41:04 +0800

    added membercount command

---
+++ commands/info/membercount.js
@@ -0,0 +1,28 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "membercount",
+  aliases: ["members"],
+  description: "Tells you the amount of members that are in your server.",
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    let { guild } = message;
+    let memberCount = guild.memberCount;
+
+    const embed = new MessageEmbed()
+      .setTitle("Members")
+      .setDescription(memberCount)
+      .setColor(client.color)
+      .setFooter(
+        client.user.username,
+        client.user.displayAvatarURL({ dynamic: true })
+      )
+      .setTimestamp();
+    message.channel.send(embed);
+  },
+};
