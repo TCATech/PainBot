d2832db6f03bb6e8198a5f544055c76efa1090aa
NotTCA <realtcatech@gmail.com>
2022-04-16 21:49:36 +0800

    added tictactoe command

---
+++ commands/games/tictactoe.js
@@ -0,0 +1,16 @@
+const { Client, Message } = require("discord.js");
+const TicTacToe = require("discord-tictactoe");
+const game = new TicTacToe({ language: "en" });
+
+module.exports = {
+  name: "tictactoe",
+  description: "Play TicTacToe against someone else, or with an AI.",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    game.handleMessage(message);
+  },
+};
