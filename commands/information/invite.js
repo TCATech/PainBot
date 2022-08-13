9c7455e18b3e62cd0a5218859042767dca1a6ad9
Not TCA <realtcatech@gmail.com>
2022-08-13 08:25:26 +0800

    added invite command

---
+++ commands/information/invite.js
@@ -0,0 +1,37 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  description: "Invite PainBot to your server.",
+  aliases: ["add", "invitebot", "invite-bot", "inviteme", "invite-me"],
+  run: async (client, message, args) => {
+    const embed = new EmbedBuilder()
+      .setTitle(`Invite me to your server`)
+      .setURL(
+        `https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1075214&scope=bot`
+      )
+      .setColor(client.config.color);
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setEmoji("➕")
+        .setStyle(5)
+        .setLabel("Invite")
+        .setURL(
+          `https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1075214&scope=bot`
+        ),
+      new ButtonBuilder()
+        .setEmoji("1007806571360825344")
+        .setStyle(5)
+        .setLabel("Invite with slash commands")
+        .setURL(
+          `https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1075214&scope=bot%20applications.commands`
+        )
+    );
+
+    message.channel.send({
+      embeds: [embed],
+      components: [row],
+    });
+  },
+};
