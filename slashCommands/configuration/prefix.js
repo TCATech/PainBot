5ef0c8c8f507fac1e53a3ea3d466e04be00db3fd
Not TCA <realtcatech@gmail.com>
2022-08-26 16:45:18 +0800

    added max length to prefix option

--- slashCommands/configuration/prefix.js
+++ slashCommands/configuration/prefix.js
@@ -13,21 +13,12 @@ module.exports = {
       option
         .setName("new_prefix")
         .setDescription("The new prefix.")
+        .setMaxLength(5)
         .setRequired(true)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
   run: async (client, interaction) => {
     const arg = interaction.options.getString("new_prefix");
-
-    if (arg.length > 5)
-      return interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setTitle(`❌ Prefix has to be less than 5 characters long.`)
-            .setColor("Red"),
-        ],
-        ephemeral: true,
-      });
 
     client.settings.set(interaction.guild.id, arg, "prefix");
 
